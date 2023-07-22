50f19ac85f9edd4af00b2a20ab3b5fcde05b46e3
Guilherme Linden <glinden@wiley.com>
2023-07-22 10:02:15 -0300

    better file order

--- main.tf
+++ main.tf
@@ -6,6 +6,25 @@ resource "aws_vpc" "devops" {
   }
 }
 
+resource "aws_subnet" "private" {
+  count             = length(data.aws_availability_zones.available_zones.names)
+  vpc_id            = aws_vpc.devops.id
+  cidr_block        = cidrsubnet(aws_vpc.devops.cidr_block, 6, count.index)
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(data.aws_availability_zones.available_zones.names)
+  vpc_id                  = aws_vpc.devops.id
+  cidr_block              = cidrsubnet(cidrsubnet(aws_vpc.devops.cidr_block, 2, 2), 6, count.index)
+  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Public = "true"
+  }
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.devops.id
 
@@ -25,25 +44,6 @@ resource "aws_route_table" "public" {
   tags = {
     Name = "public"
   }
-}
-
-resource "aws_subnet" "public" {
-  count                   = length(data.aws_availability_zones.available_zones.names)
-  vpc_id                  = aws_vpc.devops.id
-  cidr_block              = cidrsubnet(cidrsubnet(aws_vpc.devops.cidr_block, 2, 2), 6, count.index)
-  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Public = "true"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count             = length(data.aws_availability_zones.available_zones.names)
-  vpc_id            = aws_vpc.devops.id
-  cidr_block        = cidrsubnet(aws_vpc.devops.cidr_block, 6, count.index)
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
 }
 
 resource "aws_route_table_association" "public" {
